6367db777da44a60647f47ced157682f57e7f5f1
mazn <imzn77@gmail.com>
2024-12-24 13:09:17 +0300

    new

--- Assets/Scripts/opensafe.cs
+++ Assets/Scripts/opensafe.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class opensafe : MonoBehaviour
 {
    public Animator m;
+    [Header("Audio Settings")]
+   [SerializeField] AudioClip SafeOpenSound;
+   [SerializeField] AudioSource audio;
+   [SerializeField] GameObject note;
+
 
     public void SafeOpen()
     {
+        audio.PlayOneShot(SafeOpenSound);
         m.SetBool("Isopen", true);
+        note.SetActive(true);
     }
 }
